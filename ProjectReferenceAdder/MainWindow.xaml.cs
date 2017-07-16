0b799bb921c824db60d3c6d48d218ed0d559da7f
Michael Koslap <michael.koslap@gmail.com>
2017-07-16 13:03:29 -0400

    Fixed up the namespace handling for the reference changer

--- ProjectReferenceAdder/MainWindow.xaml.cs
+++ ProjectReferenceAdder/MainWindow.xaml.cs
@@ -114,43 +114,36 @@ namespace XmlTagRemover
                 {
                     // Pull the current csproj into an xdocument
                     XDocument lxd_Document;
+                    XNamespace lxn_Namespace;
                     XmlNamespaceManager lxnm_NamespaceMgr;
-                    using (XmlReader xmlReader = XmlReader.Create(ls_FileName))
+                    using (XmlReader lxr_Reader = XmlReader.Create(ls_FileName))
                     {
-                        lxd_Document = XDocument.Load(xmlReader);
-                        lxnm_NamespaceMgr = new XmlNamespaceManager(xmlReader.NameTable);
-                        lxnm_NamespaceMgr.AddNamespace("prefix", "http://schemas.microsoft.com/developer/msbuild/2003");
+                        lxd_Document = XDocument.Load(lxr_Reader);
+                        lxn_Namespace = lxd_Document.Root.Name.Namespace;
+                        lxnm_NamespaceMgr = new XmlNamespaceManager(lxr_Reader.NameTable);
+                        lxnm_NamespaceMgr.AddNamespace("prefix", lxn_Namespace.NamespaceName);
                     }
+
+                    // Get the list of nodes that contain the references and extract the Include tags
+                    XElement lxe_RefParent = lxd_Document.Root.XPathSelectElement("//prefix:Reference", lxnm_NamespaceMgr).Parent;
+                    XElement[] lxea_References = lxd_Document.Root.XPathSelectElements("//prefix:Reference[@Include]", lxnm_NamespaceMgr).ToArray();
 
                     // Based on the mode provided, we need to handle the contents of the file differently
                     if (ae_Mode == ReferenceChangeMode.Add)
                     {
-                        // Get the list of nodes that contain the references and extract the Include tags
-                        XElement[] lxea_References = lxd_Document.Root.XPathSelectElements("//prefix:Reference[@Include]/@Include", lxnm_NamespaceMgr).ToArray();
-
-                        // For each of the references
-                        XmlNode lxn_ref_node = lxd_document.CreateNode(XmlNodeType.Element, "Reference", "http://schemas.microsoft.com/developer/msbuild/2003");
-
-                        XmlAttribute lxa_ref_include = lxd_document.CreateAttribute("Include");
-                        lxa_ref_include.Value = ls_temp + ", Version=1.0.0.0, Culture=neutral, processorArchitecture=MSIL";
-                        lxn_ref_node.Attributes.Append(lxa_ref_include);
-
-                        XmlNode lxn_specific_version = lxd_document.CreateNode(XmlNodeType.Element, "SpecificVersion", "http://schemas.microsoft.com/developer/msbuild/2003");
-                        lxn_specific_version.InnerText = "False";
-                        lxn_ref_node.AppendChild(lxn_specific_version);
-
-                        XmlNode lxn_private = lxd_document.CreateNode(XmlNodeType.Element, "Private", "http://schemas.microsoft.com/developer/msbuild/2003");
-                        lxn_private.InnerText = "False";
-                        lxn_ref_node.AppendChild(lxn_private);
-
-                        lxn_node.AppendChild(lxn_ref_node);
+                        // Check each reference we were given. If a Reference tag does not already exist for it, add a new one
+                        foreach (string ls_Reference in lsa_ReferenceList)
+                        {
+                            if (!lxea_References.Any(lxe_Reference => lxe_Reference.Attribute("Include").Value.Equals(ls_Reference)))
+                            {
+                                XElement lxe_NewReference = new XElement(lxn_Namespace + "Reference", new XAttribute("Include", ls_Reference));
+                                lxe_RefParent.Add(lxe_NewReference);
+                            }
+                        }
                     }
                     else if (ae_Mode == ReferenceChangeMode.Remove)
                     {
-                        // Get the list of nodes that contain the references
-                        XElement[] lxea_References = lxd_Document.Root.XPathSelectElements("//prefix:Reference[@Include]", lxnm_NamespaceMgr).ToArray();
-
-                        // Check each reference node to see if the Include points to one of the provided references and remove it if so
+                        // Check each Reference node to see if the Include points to one of the provided references and remove it if so
                         foreach (XElement lxe_Reference in lxea_References)
                         {
                             string ls_AttributeValue = lxe_Reference.Attribute("Include").Value;
@@ -162,10 +155,9 @@ namespace XmlTagRemover
                     {
                         break;
                     }
-                    
-                    // Save the doc. We do this twice to get rid of blank lines that the remove causes
-                    lxd_Document.Save(ls_FileName);
 
+                    // Save the doc. We do so twice to fix the formatting
+                    lxd_Document.Save(ls_FileName);
                     lxd_Document = XDocument.Load(ls_FileName);
                     lxd_Document.Save(ls_FileName);
                 }
